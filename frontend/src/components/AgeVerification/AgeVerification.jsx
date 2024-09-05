5ee23bb0bb75e0f0edc5e89c8139258a7e3f5247
Kareem Sasa <kareemsasa3@gmail.com>
2024-09-05 08:23:28 -0500

    minor changes to AgeVerification component

--- frontend/src/components/AgeVerification/AgeVerification.jsx
+++ frontend/src/components/AgeVerification/AgeVerification.jsx
@@ -1,43 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
-import './AgeVerification.css'; // Import your custom CSS for styling the modal
+import './AgeVerification.css';
 
 const AgeVerification = ({ onConfirm }) => {
   const [isVerified, setIsVerified] = useState(false);
-
-  // Duration in milliseconds (1 hour)
+  const history = useHistory();
   const verificationDuration = 60 * 60 * 1000; // 1 hour
 
   useEffect(() => {
     const storedExpiryTime = localStorage.getItem('verificationExpiryTime');
-    const ageVerified = localStorage.getItem('ageVerified');
-    const currentTime = new Date().getTime();
+    const ageVerified = JSON.parse(localStorage.getItem('ageVerified'));
+    const currentTime = Date.now();
 
     if (storedExpiryTime && ageVerified === 'true' && currentTime < storedExpiryTime) {
       setIsVerified(true);
-      onConfirm(); // Automatically confirm if already verified and not expired
+      onConfirm();
     } else {
-      // If not verified or expired, reset verification status
       localStorage.removeItem('ageVerified');
       localStorage.removeItem('verificationExpiryTime');
     }
   }, [onConfirm]);
 
   const handleConfirm = () => {
+    const currentTime = Date.now();
     setIsVerified(true);
-    const currentTime = new Date().getTime();
-    localStorage.setItem('ageVerified', 'true');
-    localStorage.setItem('verificationExpiryTime', currentTime + verificationDuration); // Set expiry time
+    localStorage.setItem('ageVerified', JSON.stringify('true'));
+    localStorage.setItem('verificationExpiryTime', currentTime + verificationDuration);
     onConfirm();
   };
 
   const handleDeny = () => {
-    window.location.href = 'https://example.com/underage'; // Redirect to an underage page or show a message
+    history.push('/underage');
   };
 
   if (isVerified) {
-    return null; // If verified, do not render the modal
+    return null;
   }
 
   return (
@@ -54,9 +53,8 @@ const AgeVerification = ({ onConfirm }) => {
   );
 };
 
-// Define prop types
 AgeVerification.propTypes = {
-  onConfirm: PropTypes.func.isRequired, // onConfirm should be a function and is required
+  onConfirm: PropTypes.func.isRequired,
 };
 
 export default AgeVerification;
